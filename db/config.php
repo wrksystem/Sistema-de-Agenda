b0289d5cfdc1a50ab28f60bcc47a8e1a29889ce1
Werik Rodrigues <werikrodrigues1@hotmail.com.br>
2023-03-24 22:10:43 -0300

    ADD conexão banco de dados.

---
+++ db/config.php
@@ -0,0 +1,9 @@
+<?php
+// Constantes de configuração de conexão com o banco de dados
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "root");
+define("SENHA", "");
+define("BANCO", "dbsisagendador");
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include("db/conexao.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
