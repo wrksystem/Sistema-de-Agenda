df891bbc4603e73b7b6e3f6031c86f4dbf373ed1
Werik Rodrigues <werikrodrigues1@hotmail.com.br>
2023-03-27 22:15:48 -0300

    ADD Bootstrap Style
    Aula 11 em 11:48 - Aplicando Bootstrap

--- index.php
+++ index.php
@@ -7,20 +7,26 @@ include("db/conexao.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/estilo-padrao.css">
     <title>Sistema de Agendamento 1.0</title>
 </head>
 <body>
     
-    <header>
-        <h1>Sistema de Agendamento 1.0</h1>
-        <nav>
-            <a href="index.php?menuop=home">Home</a> |
-            <a href="index.php?menuop=contatos">Contatos</a> |
-            <a href="index.php?menuop=tarefas">Tarefas</a> |
-            <a href="index.php?menuop=eventos">Eventos</a> |
-        </nav>
+    <header class="bg-dark">
+        <div class="container">
+            <h1>Sistema de Agendamento 1.0</h1>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a href="index.php?menuop=home">Home</a> 
+                <a href="index.php?menuop=contatos">Contatos</a> 
+                <a href="index.php?menuop=tarefas">Tarefas</a> 
+                <a href="index.php?menuop=eventos">Eventos</a> 
+            </nav>
+        </div>
     </header>
-    <hr>
+    
     <main>
         <?php
             $menuop = (isset($_GET["menuop"]))?$_GET["menuop"]:"home";
@@ -59,6 +65,7 @@ include("db/conexao.php");
             }
         ?>
     </main>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 </body>
 </html>
